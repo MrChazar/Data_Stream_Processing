4829d31b408d20c5f42dd483027c45ac2e05fc49
Jacob <jakubwiesniak102@gmail.com>
2024-03-06 23:39:32 +0100

    LAB1 update

--- LAB_1/exercise_1.py
+++ LAB_1/exercise_1.py
@@ -25,44 +25,39 @@ fig.suptitle('Wykresy sygnałów')
 fs = 1000  # Częstotliwość próbkowania
 T = 1     # Czas trwania sygnału w sekundach
 t = np.linspace(0, T, T*fs, endpoint=False)
+f = 5 # częstotliwość sygnału
 
 # generowanie sygnału
 # sinus
-f_sin = 5 # częstotliwość sygnału
-x = np.sin(2*np.pi*f_sin*t)
-ax[0].plot(t, x)
+y = np.sin(2*np.pi*f*t)
+ax[0].plot(t, y)
 ax[0].set_title('sinus')
 
 # prostokąt
-f_rect = 5 # częstotliwość sygnału
-x = scp.signal.square(2*np.pi*f_rect*t)
-ax[1].plot(t, x)
+y = scp.signal.square(2*np.pi*f*t)
+ax[1].plot(t, y)
 ax[1].set_title('prostokąt')
 
 # piłokształtny
-f_saw = 5 # częstotliwość sygnału
-x = scp.signal.sawtooth(2*np.pi*f_saw*t)
-ax[2].plot(t, x)
+y = scp.signal.sawtooth(2*np.pi*f*t)
+ax[2].plot(t, y)
 ax[2].set_title('piłokształtny')
 
 # świergotliwy
 f0 = 1
 f1 = 100
-t_chirp = np.linspace(0, T, T*fs)
-x = scp.signal.chirp(t_chirp, f0, T, f1)
-ax[3].plot(t, x)
+y = scp.signal.chirp(t, f0, T, f1)
+ax[3].plot(t, y)
 ax[3].set_title('świergotliwy')
 
 # Superpozycja funkcji sinus i cosinus z samodzielnie dobranymi parametrami
-f_superpos = 3
-x = np.sin(2*np.pi*f_superpos*t) + np.cos(2*np.pi*2*f_superpos*t)
-ax[4].plot(t, x)
+y = np.sin(2*np.pi*f*t) + np.cos(2*np.pi*2*f*t)
+ax[4].plot(t, y)
 ax[4].set_title('superpozycja sinus i cosinus')
 
 # Impuls jednostkowy
-x = np.zeros_like(t)
-x[0] = 1
-ax[5].plot(t, x)
+y = scp.signal.unit_impulse(t.shape, idx='mid')
+ax[5].plot(t, y)
 ax[5].set_title('impuls jednostkowy')
 
 plt.tight_layout()
